7acfeb5a97491322f2529a138ed2b090cb1090b6
Vikram0501 <138388022+Vikram0501@users.noreply.github.com>
2025-05-14 23:18:15 +0200

    Add files via upload

---
+++ Main files/FriendRequestAdapter.java
@@ -0,0 +1,52 @@
+package com.example.testing;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class FriendRequestAdapter extends RecyclerView.Adapter<FriendRequestAdapter.MyViewHolder>{
+    Context context;
+    ArrayList<User> FriendRequests;
+    public FriendRequestAdapter(Context context, ArrayList<User> FriendRequests){
+        this.context = context;
+        this.FriendRequests = FriendRequests;
+    }
+    @NonNull
+    @Override
+    public FriendRequestAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.requests_main, parent, false);
+
+        return new FriendRequestAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FriendRequestAdapter.MyViewHolder holder, int position) {
+        holder.username.setText(FriendRequests.get(position).getUsername());
+        //holder.profilepic.
+    }
+
+    @Override
+    public int getItemCount() {
+        return FriendRequests.size();
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView username;
+        ImageView profilepic;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            username = itemView.findViewById(R.id.username_request);
+            //profilepic = itemView.findViewById(R.id.profile_image_requests);
+        }
+    }
+}
