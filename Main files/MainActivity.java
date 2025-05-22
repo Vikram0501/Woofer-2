ad32c3331fb8c28f56030de7a184cc5a1320388d
Vikram0501 <138388022+Vikram0501@users.noreply.github.com>
2025-05-22 11:45:28 +0200

    Add files via upload

--- Main files/MainActivity.java
+++ Main files/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.testing;
+package com.example.woofer;
 
 import android.content.Intent;
 import android.net.Uri;
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -21,7 +22,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.testing.Post;
+import com.example.woofer.Post;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.json.JSONArray;
@@ -277,6 +278,19 @@ public class MainActivity extends AppCompatActivity implements RequestInt, PostI
                 }
             });
         }
+        SearchView searchView = findViewById(R.id.searchView);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 
     public void profilepage(View v){
@@ -528,7 +542,7 @@ public class MainActivity extends AppCompatActivity implements RequestInt, PostI
 
     @Override
     public void viewpost(int pos) {
-        setContentView(R.layout.personal_profile_main);
+        profilepage(null);
     }
 
     @Override
@@ -654,7 +668,7 @@ public class MainActivity extends AppCompatActivity implements RequestInt, PostI
         void onError(Exception e);
     }
 
-    protected  void getfriends(Integer user_id, MainActivity.FriendCallback callback){
+    protected  void getfriends(Integer user_id, FriendCallback callback){
         ArrayList<Integer> friends = new ArrayList<>();
         String url = "https://lamp.ms.wits.ac.za/home/s2798790/importfriends.php?user_id=" + user_id;
         OkHttpClient client = new OkHttpClient();
@@ -716,7 +730,7 @@ public class MainActivity extends AppCompatActivity implements RequestInt, PostI
         void onError(Exception e);
     }
 
-    protected void getuser(Integer user_id, MainActivity.UserCallback callback){
+    protected void getuser(Integer user_id, UserCallback callback){
         User user = new User(0, null, null);
         String url = "https://lamp.ms.wits.ac.za/home/s2798790/getuser.php?user_id=" + user_id;
         OkHttpClient client = new OkHttpClient();
